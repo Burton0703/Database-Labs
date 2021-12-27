df51094e24014818abc0e609f374b7a420d80318
jiabao Zhang <74135859+Burton0703@users.noreply.github.com>
2021-12-27 16:17:01 +0200

    Delete lab2 directory

--- lab2/view.py
+++
@@ -1,6 +0,0 @@
-class View:
-
-    def display_item_already_stored_error(item, err):
-        print('++++++++++++++++++++++++++++++++++++++++')
-        print(f'Hey! we already have {item.upper()} in our database.')
-        print('++++++++++++++++++++++++++++++++++++++++')
